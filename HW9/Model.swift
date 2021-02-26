d36067893da1320ef2f458a0e8a7aa529f98c131
Nikita Zharinov <nikita.zharinov@gmail.com>
2021-02-26 10:30:14 +0100

    Initial Commit with Model

---
+++ HW9/Model.swift
@@ -0,0 +1,36 @@
+//
+//  Model.swift
+//  HW9
+//
+//  Created by Nikita Zharinov on 25/02/2021.
+//
+struct TypeAnimation {
+    let nameAnimation: String
+    let curve: String
+    let force: Float
+    let delay: Float
+    let duration: Float
+}
+
+extension TypeAnimation {
+    static func getNewAnimation() -> TypeAnimation {
+        TypeAnimation(
+            nameAnimation: AnimationName().animationName[
+                Int.random(in: 0..<AnimationName().animationName.count)
+            ],
+            curve: AnimationName().curveType[
+                Int.random(in: 0..<AnimationName().curveType.count)
+            ],
+            force: Float.random(in: 1...2),
+            delay: Float.random(in: 1...2),
+            duration: Float.random(in: 1...2)
+        )
+    }
+}
+
+//MARK: - Get types of animation and cuyrves from DB
+class AnimationName {
+    let animationName = ["shake", "pop", "flash", "wobble", "fall", "morph", "swing"]
+    let curveType = ["spring", "easeIn", "easeOut"]
+}
+
--- HW9/ViewController.swift
+++ HW9/ViewController.swift
@@ -5,15 +5,41 @@
 //  Created by Nikita Zharinov on 25/02/2021.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var animationLabel: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
+    @IBOutlet var delayLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var springAnimatedView: SpringView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func animationButton(_ sender: SpringButton) {
+        let animation = TypeAnimation.getNewAnimation()
+        springAnimatedView.animation = animation.nameAnimation
+        springAnimatedView.curve = animation.curve
+        springAnimatedView.force = CGFloat(animation.force)
+        springAnimatedView.delay = CGFloat(animation.delay)
+        springAnimatedView.duration = CGFloat(animation.duration)
+        
+        animationLabel.text = "Animation : \(animation.nameAnimation)"
+        curveLabel.text = "Curve : \(animation.curve)"
+        forceLabel.text = "Force : \(fortmatString(from: animation.force))"
+        delayLabel.text = "Delay : \(fortmatString(from: animation.delay))"
+        durationLabel.text = "Duration : \(fortmatString(from: animation.duration))"
+        sender.setTitle("Run \(animation.nameAnimation)", for: .normal)
+
+        springAnimatedView.animate()
     }
-
-
+    
+    private func fortmatString(from random: Float) -> String {
+        String(format: "%.2f", random)
+    }
+    
+    private func getAnimationParamenters () {
+        
+    }
 }
 
